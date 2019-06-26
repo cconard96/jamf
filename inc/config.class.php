f76fed7adafd6af16ddccd818dced281c482d869
cconard96 <cconard96@gmail.com>
2019-06-25 20:23:54 -0400

    Support partial syncs Respect sync interval and last sync date Closes #4

--- inc/config.class.php
+++ inc/config.class.php
@@ -32,7 +32,7 @@ class PluginJamfConfig extends CommonDBTM
       if (!Session::haveRight("config", UPDATE)) {
          return false;
       }
-      $jamf_config = self::getConfig(true);
+      $config = self::getConfig(true);
       echo "<form name='form' action=\"".Toolbox::getItemTypeFormURL('Config')."\" method='post'>";
       echo "<div class='center' id='tabsbody'>";
       echo "<table class='tab_cadre_fixe'><thead>";
@@ -41,12 +41,16 @@ class PluginJamfConfig extends CommonDBTM
       echo "<td>";
       echo "<input type='hidden' name='config_class' value='".__CLASS__."'>";
       echo "<input type='hidden' name='config_context' value='plugin:Jamf'>";
-      echo Html::input('jssserver', ['value' => $jamf_config['jssserver']]);
+      echo Html::input('jssserver', [
+         'value' => $config['jssserver']
+      ]);
       echo "</td><td>".__('JSS User:', 'jamf')."</td><td>";
-      echo Html::input('jssuser', ['value' => $jamf_config['jssuser']]);
+      echo Html::input('jssuser', [
+         'value' => $config['jssuser']
+      ]);
       echo "</td></tr><tr><td>".__('JSS Password:', 'jamf')."</td><td>";
       echo Html::input('jsspassword', ['type' => 'password']);
-      $msg = (isset($jamf_config['jsspassword']) && strlen($jamf_config['jsspassword'])) ? __('Password set') : '';
+      $msg = (isset($config['jsspassword']) && strlen($config['jsspassword'])) ? __('Password set') : '';
       echo "</td><td>{$msg}</td></tr>";
       echo "</table>";
 
@@ -56,38 +60,38 @@ class PluginJamfConfig extends CommonDBTM
       echo "<tr><td>" . __('Sync Interval (minutes):', 'jamf') . "</td>";
       echo "<td>";
       Dropdown::showNumber('sync_interval', [
-         'value'  => $jamf_config['sync_interval']
+         'value'  => isset($config['sync_interval']) ? $config['sync_interval'] : 15
       ]);
       echo "</td></tr>";
 
       echo "<tr><td>" . __('Sync General:', 'jamf') . "</td>";
       echo "<td>";
-      Dropdown::showYesNo('sync_general', $jamf_config['sync_general']);
+      Dropdown::showYesNo('sync_general', isset($config['sync_general']) ? $config['sync_general'] : false);
       echo "</td>";
 
       echo "<td>" . __('Sync OS:', 'jamf') . "</td>";
       echo "<td>";
-      Dropdown::showYesNo('sync_os', $jamf_config['sync_os']);
+      Dropdown::showYesNo('sync_os', isset($config['sync_os']) ? $config['sync_os'] : false);
       echo "</td></tr>";
 
       echo "<tr><td>" . __('Sync Software:', 'jamf') . "</td>";
       echo "<td>";
-      Dropdown::showYesNo('sync_software', $jamf_config['sync_software']);
+      Dropdown::showYesNo('sync_software', isset($config['sync_software']) ? $config['sync_software'] : false);
       echo "</td>";
 
       echo "<td>" . __('Sync Financial:', 'jamf') . "</td>";
       echo "<td>";
-      Dropdown::showYesNo('sync_financial', $jamf_config['sync_financial']);
+      Dropdown::showYesNo('sync_financial', isset($config['sync_financial']) ? $config['sync_financial'] : false);
       echo "</td></tr>";
 
       echo "<tr><td>" . __('Sync Components:', 'jamf') . "</td>";
       echo "<td>";
-      Dropdown::showYesNo('sync_components', $jamf_config['sync_components']);
+      Dropdown::showYesNo('sync_components', isset($config['sync_components']) ? $config['sync_components'] : false);
       echo "</td></tr>";
 
       echo "<tr><td>" . __('Sync User:', 'jamf') . "</td>";
       echo "<td>";
-      Dropdown::showYesNo('sync_user', $jamf_config['sync_user']);
+      Dropdown::showYesNo('sync_user', isset($config['sync_user']) ? $config['sync_user'] : false);
       echo "</td>";
 
       echo "<td>" . __('User Sync Mode:', 'jamf') . "</td>";
@@ -95,12 +99,12 @@ class PluginJamfConfig extends CommonDBTM
       Dropdown::showFromArray('user_sync_mode', [
          'email' => __('Email'),
          'username' => __('Username')
-      ]);
+      ], ['value' => isset($config['user_sync_mode']) ? $config['user_sync_mode'] : 'email']);
       echo "</td></tr>";
 
       echo "<tr><td>" . __('Auto Import:', 'jamf') . "</td>";
       echo "<td>";
-      Dropdown::showYesNo('autoimport', $jamf_config['autoimport']);
+      Dropdown::showYesNo('autoimport', isset($config['autoimport']) ? $config['autoimport'] : false);
       echo "</td></tr>";
 
       echo "<table class='tab_cadre_fixe'><thead>";
@@ -109,20 +113,23 @@ class PluginJamfConfig extends CommonDBTM
       echo "<td>";
       Dropdown::show('Manufacturer', [
          'name' => 'default_manufacturer',
-         'value' => $jamf_config['default_manufacturer']]);
+         'value' => isset($config['default_manufacturer']) ? $config['default_manufacturer'] : false
+      ]);
       echo "</td><td>".__('iPhone Type:', 'jamf')."</td><td>";
       Dropdown::show('PhoneType', [
          'name' => 'iphone_type',
-         'value' => $jamf_config['iphone_type']]);
+         'value' => isset($config['iphone_type']) ? $config['iphone_type'] : false
+      ]);
       echo "</td></tr><tr><td>".__('iPad Type:', 'jamf')."</td><td>";
       Dropdown::show('ComputerType', [
          'name' => 'ipad_type',
-         'value' => $jamf_config['ipad_type']]);
-      $msg = isset($jamf_config['jsspassword']) ? __('Password set') : '';
+         'value' => isset($config['ipad_type']) ? $config['autoimport'] : false
+      ]);
       echo "</td><td>".__('AppleTV Type', 'jamf')."</td><td>";
       Dropdown::show('ComputerType', [
          'name' => 'appletv_type',
-         'value' => $jamf_config['appletv_type']]);
+         'value' => isset($config['appletv_type']) ? $config['appletv_type'] : false
+      ]);
       echo "</td></tr></table>";
 
       echo "<table class='tab_cadre_fixe'>";
