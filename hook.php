057e4ee1814ec68bb10d813d7332df83942a48bb
cconard96 <cconard96@gmail.com>
2020-06-15 20:47:52 -0400

    Add lost mode, managed, and supervices mobile device counts

--- hook.php
+++ hook.php
@@ -469,6 +469,7 @@ function plugin_jamf_dashboardCards()
 {
    $cards = [];
    $cards = array_merge($cards, PluginJamfExtensionAttribute::dashboardCards());
+   $cards = array_merge($cards, PluginJamfMobileDevice::dashboardCards());
 
    return $cards;
 }
